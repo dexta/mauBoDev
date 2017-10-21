5dc4832ed8253f544ea33dd1c510039f66f88caa
dexta <dexta.de@gmail.com>
2017-10-21 20:02:11 +0200

    add log to export path

--- js/exports.js
+++ js/exports.js
@@ -2,23 +2,29 @@ var debugMp = {};
 var tasks = [];
 
 function mp3ncode(buffer,callback) {
+  __log("start encoding");
   var encoder = new Mp3LameEncoder(44100, 128);
   encoder.encode(buffer);
   var fmp3 = encoder.finish("audio/mp3");
+  __log("stop encoding");
   callback(fmp3);
   // saveAs(fmp3,"testEncode.mp3");
 }
 
 function makeZipFile(data,filename,options) {
+  __log("prepare zip archive "+filename);
   var zip = new JSZip();
   var fol = zip.folder("mp3xport");
+  __log("add folder mp3Export");
   for(var dm in data) {
     var blob = data[dm].bURL;
     var mpid = (options.no)? data[dm].lPos : data[dm].name;
     fol.file(mpid+".mp3", blob, {base64: false});
+    __log("add file "+mpid+".mp3");
   }  
   zip.generateAsync({type:"blob"}).then(function(content) {
     // see FileSaver.js
+    __log("start download ...");
     saveAs(content, filename);
   });
 }
